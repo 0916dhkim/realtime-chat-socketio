e757e9d137f3eaab0ec8bcb1dce4c540593046e9
Donghyeon Kim <0916dhkim@gmail.com>
2021-05-18 00:48:40 -0400

    Update Signup Page

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,16 +1,140 @@
-import React, { useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import { connect } from "react-redux";
 import {
-  Grid,
-  Box,
-  Typography,
   Button,
   FormControl,
-  TextField,
   FormHelperText,
+  TextField,
+  styled,
 } from "@material-ui/core";
+import React, { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
+
+import bannerImage from './assets/images/bg-img.png';
+import bubbleVector from './assets/images/bubble.svg';
+import { connect } from "react-redux";
 import { register } from "./store/utils/thunkCreators";
+
+const Root = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+
+  [theme.breakpoints.up('lg')]: {
+    gridTemplateColumns: '4fr 6fr',
+  }
+}));
+
+const SideBanner = styled('div')(({ theme }) => ({
+  background: `top center / cover no-repeat url(${bannerImage})`,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  minHeight: '100vh',
+  position: 'relative',
+
+  '&::before': {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    opacity: 0.8532,
+    background: `linear-gradient(${theme.palette.primary.main}, ${theme.palette.primary.light})`,
+  },
+
+  '& *': {
+    zIndex: 1,
+  },
+
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
+  }
+}));
+
+const BannerText = styled('span')(({ theme }) => ({
+  fontSize: '26pt',
+  lineHeight: '40px',
+  textAlign: 'center',
+  marginTop: 39,
+  color: theme.palette.getContrastText(theme.palette.primary.main),
+}));
+
+const Container = styled('div')({
+  padding: '30px 42px',
+  display: 'flex',
+  gap: 86,
+  flexDirection: 'column',
+  alignItems: 'stretch',
+  justifyContent: 'flex-start',
+});
+
+const Heading = styled('div')({
+  display: 'flex',
+  gap: 30,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+});
+
+const HeadingText = styled('span')(({ theme }) => ({
+  fontSize: '14pt',
+  color: theme.palette.secondary.main,
+}));
+
+const CreateAccountButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.primary.main,
+  fontSize: '14pt',
+  fontWeight: 600,
+  padding: '16px 33px',
+}));
+
+const SignupInput = styled(TextField)(({ theme }) => ({
+  '& .MuiInputLabel-root': {
+    color: theme.palette.secondary.main,
+    fontSize: '14pt',
+  },
+
+  '& .MuiInputLabel-formControl': {
+    transform: 'translate(0, 40px)',
+  },
+
+  '& .MuiInputLabel-shrink': {
+    transform: 'translate(0, 1.5px)',
+  },
+
+  '& label + .MuiInput-formControl': {
+    marginTop: 40,
+  },
+}));
+
+const FormBase = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+});
+
+const FormContent = styled('form')({
+  flex: '0 1 380px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+});
+
+const FormTitle = styled('span')(({ theme }) => ({
+  fontSize: '26pt',
+  fontWeight: 600,
+  lineHeight: '40px',
+}));
+
+const SignupButton = styled(Button)(({ theme }) => ({
+  alignSelf: 'center',
+  fontSize: '16pt',
+  fontWeight: 'bold',
+  lineHeight: '20px',
+  padding: '18px 60px',
+}));
 
 const Login = (props) => {
   const history = useHistory();
@@ -37,73 +161,76 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
+    <Root>
+      <SideBanner>
+        <img src={bubbleVector} alt="" />
+        <BannerText >Converse with anyone<br />with any language</BannerText>
+      </SideBanner>
+      <Container>
+        <Heading>
+          <HeadingText>Already have an account?</HeadingText>
+          <CreateAccountButton onClick={() => history.push("/login")} variant="contained">
+            Login
+          </CreateAccountButton>
+        </Heading>
+        <FormBase>
+          <FormContent onSubmit={handleRegister}>
+            <FormTitle>Create an account.</FormTitle>
+            <FormControl>
+              <SignupInput
+                aria-label="username"
+                label="Username"
+                name="username"
+                type="text"
+                required
+                style={{ marginTop: 12 }}
+              />
+            </FormControl>
+            <FormControl>
+              <SignupInput
+                label="E-mail address"
+                aria-label="e-mail address"
+                type="email"
+                name="email"
+                required
+                style={{ marginTop: 40 }}
+              />
+            </FormControl>
+            <FormControl error={!!formErrorMessage.confirmPassword}>
+              <SignupInput
+                aria-label="password"
+                label="Password"
+                type="password"
+                inputProps={{ minLength: 6 }}
+                name="password"
+                required
+                style={{ marginTop: 40 }}
+              />
+              <FormHelperText>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
+            <FormControl error={!!formErrorMessage.confirmPassword}>
+              <SignupInput
+                label="Confirm Password"
+                aria-label="confirm password"
+                type="password"
+                inputProps={{ minLength: 6 }}
+                name="confirmPassword"
+                required
+                style={{ marginTop: 40, marginBottom: 40 }}
+              />
+              <FormHelperText>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
+            <SignupButton type="submit" variant="contained" color="primary" disableElevation>
               Create
-            </Button>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
+            </SignupButton>
+          </FormContent>
+        </FormBase>
+      </Container>
+    </Root>
   );
 };
 
