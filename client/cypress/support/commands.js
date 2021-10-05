cfd59ef368c2c1852027984fbc3cc767fbc0e6d6
Donghyeon Kim <0916dhkim@gmail.com>
2021-10-05 11:52:06 -0400

    E2E Test Cases for Message Ordering

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,25 +1,26 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+Cypress.Commands.add("signup", (username, email, password) => {
+  cy.visit("http://localhost:3000/register");
+
+  cy.get("input[name=username]").type(username);
+  cy.get("input[name=email]").type(email);
+  cy.get("input[name=password]").type(password);
+  cy.get("input[name=confirmPassword]").type(password);
+
+  cy.contains("button", "Create").click();
+});
+
+Cypress.Commands.add("login", (username, password) => {
+  cy.visit("http://localhost:3000/");
+
+  cy.contains("button", "Login").click();
+
+  cy.get("input[name=username]").type(username);
+  cy.get("input[name=password]").type(password);
+
+  cy.contains("button", "Login").click();
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.contains("button", "Logout").click();
+});
